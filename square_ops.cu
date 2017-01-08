f06eee7520f3f2e38246b73a1dac32bb6a25b0dd
shilpi75 <shilpi.c75@gmail.com>
2017-01-08 19:36:18 +0530

    Updated

--- square_ops.cu
+++ square_ops.cu
@@ -1,5 +1,6 @@
 // Corresponding header file: /include/square_ops.h
 #include <cuda_runtime.h>
+#include "include/blur_ops.h"
 
 /* Write the code to square(blur) the image.
    2 cases as already specified.
@@ -13,7 +14,7 @@
 
 
 __global__ 
-void square(const uchar4* d_in, uchar4* d_sq, uchar4 color, size_t numRows, size_t numCols, size_t n_numRows, size_t n_numCols)
+void square(const uchar4* d_in, uchar4* d_sq, size_t numRows, size_t numCols, size_t n_numRows, size_t n_numCols, uchar4 color)
 {
  	int y = blockDim.x*blockIdx.x + threadIdx.x;	//column
 	int x = blockDim.y*blockIdx.y + threadIdx.y;	//row
@@ -30,16 +31,14 @@ void square(const uchar4* d_in, uchar4* d_sq, uchar4 color, size_t numRows, size
 }
 
 __global__ 
-void square_blurr(const uchar4* d_in, uchar4* d_sq,  int blurr_amount, const float* const d_filter, const size_t filterWidth, size_t numRows, 			size_t numCols, size_t n_numRows, size_t n_numCols)
+void square_blur(const uchar4* d_in, uchar4* d_sq, const float* const d_filter, const int filterWidth, size_t numRows, size_t numCols,      size_t n_numRows, size_t n_numCols)
 {
-	//NO BLURR AMOUNT CONCEPT ADDED. WILL ADD ACCORDING TO BLURR_OP.CU FILE
-
-	int y = blockDim.x*blockIdx.x + threadIdx.x;	//column
+	int y = blockDim.x*blockIdx.x + threadIdx.x;  //column
 	int x = blockDim.y*blockIdx.y + threadIdx.y;	//row
 	int index = x*numRows + y;										//previous index of pixel
-	int n_index = x*n_numRows + y;								//new index of pixel
+	int n_index = x*n_numRows + y;						//new index of pixel
 
-	if(y >= n_numCols || x >= n_numRows)  				//check out of bound
+	if(y >= n_numCols || x >= n_numRows)  		//check out of bound
 	  return;
 
 	if(y < numCols && x < numRows)								
@@ -55,7 +54,7 @@ void square_blurr(const uchar4* d_in, uchar4* d_sq,  int blurr_amount, const flo
 	  {
 	    for(int py = 0; py < filterWidth; py++)
 	    {
-        int row = x + px - (filterWidth/2);
+       	int row = x + px - (filterWidth/2);
         int col = y + py - (filterWidth/2);
         row = min( max(0,row), static_cast<unsigned int>(numCols-1));
         col = min( max(0,col), static_cast<unsigned int>(numRows-1));
@@ -73,13 +72,38 @@ void square_blurr(const uchar4* d_in, uchar4* d_sq,  int blurr_amount, const flo
 	n_numRows and n_numCols are the new row and column sizes
 	d_sq represents output image intensities
 */
-uchar4* square(const uchar4* const h_image, uchar4* const d_image, uchar4 color, size_t numRows, size_t numCols, 
-							size_t &n_numRows, size_t &n_numCols, bool blurr, int blurr_amount, const float* const d_filter, const size_t filterWidth)
+
+uchar4* square(uchar4* const d_image, size_t numRows, size_t numCols, size_t &n_numRows, size_t &n_numCols, uchar4 color)
 {
-	size_t size, newSize;
+	size_t newSize;
   const dim3 blockSize(64, 64, 1);  
   const dim3 gridSize(numRows/blockSize.x+1, numCols/blockSize.y+1,1);  
-  size = numRows*numCols;
+   
+  if(numCols > numRows)		//setting new cols and rows size
+  {
+    n_numRows = numCols;
+    n_numCols = numCols;
+  }
+  else
+  {
+    n_numCols = numRows; 
+    n_numRows = numRows;
+  }
+  
+	newSize = n_numRows * n_numCols;
+  uchar4* d_sq;
+  cudaMalloc(&d_sq, sizeof(uchar4)*newSize);
+	square<<<gridSize, blockSize>>>(d_image, d_sq, numRows, numCols, n_numRows, n_numCols, color);
+
+  return d_sq;
+}
+
+
+uchar4* square_blur(uchar4* const d_image, size_t numRows, size_t numCols, size_t &n_numRows, size_t &n_numCols, int blurKernelWidth, float blurKernelSigma)
+{
+	size_t newSize;
+  const dim3 blockSize(64, 64, 1);  
+  const dim3 gridSize(numRows/blockSize.x+1, numCols/blockSize.y+1,1);  
   
   if(numCols > numRows)		//setting new cols and rows size
   {
@@ -95,10 +119,17 @@ uchar4* square(const uchar4* const h_image, uchar4* const d_image, uchar4 color,
   newSize = n_numRows * n_numCols;
   uchar4* d_sq;
   cudaMalloc(&d_sq, sizeof(uchar4)*newSize);
-  if(blurr)
-  square_blurr<<<gridSize, blockSize>>>(d_image, d_sq, blurr_amount, d_filter, filterWidth, numRows, numCols, n_numRows, n_numCols);
-  else
-  square<<<gridSize, blockSize>>>(d_image, d_sq, color, numRows, numCols, n_numRows, n_numCols);
+  
+  /*setting the filter: we need to change blurkernelWidth and blurKernelSigma to change the filter */
+  float* h_filter;
+  size_t filterWidth;
+  setFilter(&h_filter, &filterWidth, blurKernelWidth, blurKernelSigma);
 
+  float *d_filter; 	//creating device copy of h_filter
+  cudaMalloc(&d_filter, sizeof(float) * blurKernelWidth * blurKernelWidth);
+  cudaMemcpy(d_filter, h_filter, sizeof(float) * blurKernelWidth * blurKernelWidth, cudaMemcpyHostToDevice);
+
+  square_blur<<<gridSize, blockSize>>>(d_image, d_sq, numRows, numCols, n_numRows, n_numCols, d_filter, filterWidth);
+  
   return d_sq;
 }
