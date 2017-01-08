9b464911bdd4df03c90b60944313ccb01432339a
vagisha-nidhi <vagisha.nda@gmail.com>
2017-01-09 02:04:12 +0530

    Mirror Code modified

--- mirror_ops.cu
+++ mirror_ops.cu
@@ -15,15 +15,11 @@
 
 unsigned char *d_red, *d_green, *d_blue;
 
-__global__
-void mirror(const unsigned char* const inputChannel,
-                   unsigned char* const outputChannel,
-                   int numRows, int numCols, bool vertical)
+__global__ 
+void mirror(const uchar4* const inputChannel, uchar4* outputChannel, int numRows, int numCols, bool vertical)
 {
-
-  __shared__ unsigned char sharedBlockA[4][4];   // 1. shared memory for reverse swap
-
-  __shared__ unsigned char sharedBlockB[4][4];   // 2. shared memory for reverse swap
+  __shared__ uchar4 sharedBlockA[4][4];   // 1. shared memory for reverse swap
+  __shared__ uchar4 sharedBlockB[4][4];   // 2. shared memory for reverse swap
 
   int col = blockIdx.x * blockDim.x + threadIdx.x;
   int row = blockIdx.y * blockDim.y + threadIdx.y;
@@ -44,108 +40,56 @@ void mirror(const unsigned char* const inputChannel,
   	int blockIdxA = blockIdx.x * blockDim.x + blockIdx.y * blockDim.y * dX;	 //  begin read
   	int blockIdxB = dX * dY - blockIdxA - blockDim.y * dX - blockDim.x;		 //  store data
   
-  	sharedBlockA[ty][tx] = inputChannel[blockIdxA + ty * dX + tx];  // linear data fetch from global memory
+  	sharedBlockA[ty][tx].x = inputChannel[blockIdxA + ty * dX + tx].x;  // linear data fetch from global memory
+  	sharedBlockA[ty][tx].y = inputChannel[blockIdxA + ty * dX + tx].y; 
+  	sharedBlockA[ty][tx].z = inputChannel[blockIdxA + ty * dX + tx].z; 
+  	
 		__syncthreads ();   // wait for all threads to reach this point
-		sharedBlockB[ty][tx] = sharedBlockA[3-ty][3-tx]; // mirror each element in the cache
+
+		sharedBlockB[ty][tx].x = sharedBlockA[3-ty][3-tx].x; // mirror each element in the cache
+		sharedBlockB[ty][tx].y = sharedBlockA[3-ty][3-tx].y;
+		sharedBlockB[ty][tx].z = sharedBlockA[3-ty][3-tx].z;
+		
 		__syncthreads();   // wait for all threads to reach this point
-
-   	outputChannel[blockIdxB + ty * dX + tx] = sharedBlockB[ty][tx];   // linear data store in global memory
+		
+		unsigned char red   = 	sharedBlockB[ty][tx].x;
+		unsigned char blue   = 	sharedBlockB[ty][tx].y;
+		unsigned char green   = 	sharedBlockB[ty][tx].z;
+		outputChannel[blockIdxB + ty * dX + tx] = make_uchar4(red,blue,green,255);   // linear data store in global memory
+   	
   }
-
-  else
+	else
   {
   	int blockIdxA = blockIdx.x * blockDim.x + blockIdx.y * blockDim.y * dX;	 //  begin read
   	int blockIdxB = blockIdx.y * blockDim.y * dX + dX*blockDim.y - blockIdx.x*blockDim.x - blockDim.x*blockDim.y; //  store data
   
-  	sharedBlockA[ty][tx] = inputChannel[blockIdxA + ty * dX + tx];  // linear data fetch from global memory
+  	sharedBlockA[ty][tx].x = inputChannel[blockIdxA + ty * dX + tx].x;  // linear data fetch from global memory
+  	sharedBlockA[ty][tx].y = inputChannel[blockIdxA + ty * dX + tx].y; 
+  	sharedBlockA[ty][tx].z = inputChannel[blockIdxA + ty * dX + tx].z; 
+ 
 		__syncthreads ();   // wait for all threads to reach this point
-		sharedBlockB[ty][tx] = sharedBlockA[ty][3-tx]; // mirror each element in the cache
+
+		sharedBlockB[ty][tx].x = sharedBlockA[ty][3-tx].x; // mirror each element in the cache
+		sharedBlockB[ty][tx].y = sharedBlockA[ty][3-tx].y;
+		sharedBlockB[ty][tx].z = sharedBlockA[ty][3-tx].z;
+	
 		__syncthreads();   // wait for all threads to reach this point
+		
+		unsigned char red   = 	sharedBlockB[ty][tx].x;
+		unsigned char blue   = 	sharedBlockB[ty][tx].y;
+		unsigned char green   = 	sharedBlockB[ty][tx].z;
 
-   	outputChannel[blockIdxB + ty * dX + tx] = sharedBlockB[ty][tx];   // linear data store in global memory
+   	outputChannel[blockIdxB + ty * dX + tx] = make_uchar4(red,blue,green,255);   // linear data store in global memory
+   	
   }
-
-}
-
-
-__global__
-void separateChannels(const uchar4* const inputImageRGBA,
-                      int numRows,
-                      int numCols,
-                      unsigned char* const redChannel,
-                      unsigned char* const greenChannel,
-                      unsigned char* const blueChannel)
-{
-
-  
-  int absolute_image_position_x = blockDim.x * blockIdx.x + threadIdx.x;
-  int absolute_image_position_y = blockDim.y * blockIdx.y + threadIdx.y;
-
-  if ( absolute_image_position_x >= numCols ||
-      absolute_image_position_y >= numRows )
-  {
-       return;
-  }
-  
-  int thread_1D_pos = absolute_image_position_y * numCols + absolute_image_position_x;
-
-  redChannel[thread_1D_pos] = inputImageRGBA[thread_1D_pos].x;
-  greenChannel[thread_1D_pos] = inputImageRGBA[thread_1D_pos].y;
-  blueChannel[thread_1D_pos] = inputImageRGBA[thread_1D_pos].z;
-
-}
-
-__global__
-void recombineChannels(const unsigned char* const redChannel,
-                       const unsigned char* const greenChannel,
-                       const unsigned char* const blueChannel,
-                       uchar4* const outputImageRGBA,
-                       int numRows,
-                       int numCols)
-{
-  const int2 thread_2D_pos = make_int2( blockIdx.x * blockDim.x + threadIdx.x,
-                                        blockIdx.y * blockDim.y + threadIdx.y);
-
-  const int thread_1D_pos = thread_2D_pos.y * numCols + thread_2D_pos.x;
-
-  if (thread_2D_pos.x >= numCols || thread_2D_pos.y >= numRows)
-    return;
-
-  unsigned char red   = redChannel[thread_1D_pos];
-  unsigned char green = greenChannel[thread_1D_pos];
-  unsigned char blue  = blueChannel[thread_1D_pos];
-
-  //Alpha should be 255 for no transparency
-  uchar4 outputPixel = make_uchar4(red, green, blue, 255);
-
-  outputImageRGBA[thread_1D_pos] = outputPixel;
-}
+}         
 
 
-
-
-void allocateMemoryAndCopyToGPU(const size_t numRowsImage, const size_t numColsImage)
-{
-
-  //allocate memory for the three different channels
-  //original
-  cudaMalloc(&d_red,   sizeof(unsigned char) * numRowsImage * numColsImage);
-  cudaMalloc(&d_green, sizeof(unsigned char) * numRowsImage * numColsImage);
-  cudaMalloc(&d_blue,  sizeof(unsigned char) * numRowsImage * numColsImage);
-
-}
-
-void cleanup() {
-  cudaFree(d_red);
-  cudaFree(d_green);
-  cudaFree(d_blue);
-}
 
 uchar4* mirror_ops(const uchar4* const h_in, size_t numRows, size_t numCols, bool vertical)
 {
 	//Set reasonable block size (i.e., number of threads per block)
-
-  const dim3 blockSize(4,4,1);
+	const dim3 blockSize(4,4,1);
   //Calculate Grid SIze
   int a=numCols/blockSize.x, b=numRows/blockSize.y;	
   const dim3 gridSize(a+1,b+1,1);
@@ -160,56 +104,18 @@ uchar4* mirror_ops(const uchar4* const h_in, size_t numRows, size_t numCols, boo
   //copy input array to the GPU
   cudaMemcpy(d_inputImageRGBA, h_in, sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice);
 
-  //blurred
-  cudaMalloc(d_redBlurred, sizeof(unsigned char) * numPixels);
-  cudaMalloc(d_greenBlurred,  sizeof(unsigned char) * numPixels);
-  cudaMalloc(d_blueBlurred,   sizeof(unsigned char) * numPixels);
-  cudaMemset(*d_redBlurred,   0, sizeof(unsigned char) * numPixels);
-  cudaMemset(*d_greenBlurred, 0, sizeof(unsigned char) * numPixels);
-  cudaMemset(*d_blueBlurred,  0, sizeof(unsigned char) * numPixels);
-
-  allocateMemoryAndCopyToGPU(numRows, numCols);
-
-  //Launch a kernel for separating the RGBA image into different color channels
-  separateChannels<<<gridSize, blockSize>>>(d_inputImageRGBA, numRows, numCols, d_red,d_green, d_blue);
+  //Call mirror kernel.
+  mirror<<<gridSize, blockSize>>>(d_inputImageRGBA, d_outputImageRGBA, numRows, numCols, vertical);
 
   cudaDeviceSynchronize(); 
-
-  //Call mirror kernel here 3 times, once for each color channel.
-  mirror<<<gridSize, blockSize>>>(d_red, d_redBlurred, numRows, numCols, vertical);
-  mirror<<<gridSize, blockSize>>>(d_green, d_greenBlurred, numRows, numCols, vertical);
-  mirror<<<gridSize, blockSize>>>(d_blue, d_blueBlurred, numRows, numCols, vertical);
-
-
-  cudaDeviceSynchronize(); 
-
-  //Now we recombine the results.
-
-  recombineChannels<<<gridSize, blockSize>>>(d_redBlurred,
-                                             d_greenBlurred,
-                                             d_blueBlurred,
-                                             d_outputImageRGBA,
-                                             numRows,
-                                             numCols);
-  cudaDeviceSynchronize(); 
-
-  //cleanup memory
-  cleanup();
-  cudaFree(d_redBlurred);
-  cudaFree(d_greenBlurred);
-  cudaFree(d_blueBlurred);
-
-  cudaDeviceSynchronize(); 
-
+ 
   //Initialize memory on host for output uchar4*
   uchar4* h_out;
   h_out = (uchar4*)malloc(sizeof(uchar4) * numPixels)
 
   //copy output from device to host
   cudaMemcpy(h_out, d_outputImageRGBA, sizeof(uchar4) * numPixels, cudaMemcpyDeviceToHost);
-
-  cudaDeviceSynchronize();
-
+  
   //cleanup memory on device
   cudaFree(d_inputImageRGBA);
   cudaFree(d_outputImageRGBA);
