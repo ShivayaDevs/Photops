18b619da565132eea157321ca4790c3e1ab5fdf0
Shilpi Gupta <shilpi.c75@gmail.com>
2017-01-08 15:11:51 +0530

    Both square operations added

--- square_ops.cu
+++ square_ops.cu
@@ -17,10 +17,10 @@ void square(const uchar4* d_in, uchar4* d_sq, uchar4 color, size_t numRows, size
 {
  	int y = blockDim.x*blockIdx.x + threadIdx.x;	//column
 	int x = blockDim.y*blockIdx.y + threadIdx.y;	//row
-	int index = x*numRows + y;										//previous index of pixel
-	int n_index = x*n_numRows + y;								//new index of pixel
+	int index = x*numRows + y;			//previous index of pixel
+	int n_index = x*n_numRows + y;			//new index of pixel
 
-	if(y >= n_numCols || x >= n_numRows)  				//check out of bound
+	if(y >= n_numCols || x >= n_numRows)  		//check out of bound
 	  return;
 
 	if(y < numCols && x < numRows)								
@@ -36,34 +36,34 @@ void square_blurr(const uchar4* d_in, uchar4* d_sq,  int blurr_amount, const flo
 
 	int y = blockDim.x*blockIdx.x + threadIdx.x;	//column
 	int x = blockDim.y*blockIdx.y + threadIdx.y;	//row
-	int index = x*numRows + y;										//previous index of pixel
-	int n_index = x*n_numRows + y;								//new index of pixel
+	int index = x*numRows + y;			//previous index of pixel
+	int n_index = x*n_numRows + y;			//new index of pixel
 
-	if(y >= n_numCols || x >= n_numRows)  				//check out of bound
+	if(y >= n_numCols || x >= n_numRows)  		//check out of bound
 	  return;
 
 	if(y < numCols && x < numRows)								
 	  d_sq[n_index] = d_in[index];
 	else
 	{
-	  int prev_x = x - (n_numRows - numRows);		//finding pixel to blurr
+	  int prev_x = x - (n_numRows - numRows);	//finding pixel to blurr
 	  int prev_y = y - (n_numCols - numCols);
 	  int prev_index = prev_x * numRows + prev_y;
 
 	  uchar4 sum = make_uchar4(0,0,0,225);
-    for(int px = 0; px < filterWidth; px++)		//calculating new pixel intensity
+    	  for(int px = 0; px < filterWidth; px++)	//calculating new pixel intensity
 	  {
 	    for(int py = 0; py < filterWidth; py++)
 	    {
-        int row = x + px - (filterWidth/2);
-        int col = y + py - (filterWidth/2);
-        row = min( max(0,row), static_cast<unsigned int>(numCols-1));
-        col = min( max(0,col), static_cast<unsigned int>(numRows-1));
-        sum.x+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].x ) );
-        sum.y+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].y ) );
-        sum.z+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].z ) );
+		int row = x + px - (filterWidth/2);
+		int col = y + py - (filterWidth/2);
+		row = min( max(0,row), static_cast<unsigned int>(numCols-1));
+		col = min( max(0,col), static_cast<unsigned int>(numRows-1));
+		sum.x+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].x ) );
+		sum.y+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].y ) );
+		sum.z+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].z ) );
 	    }
-  	}
+  	  }
 
   	d_sq[n_index] = sum;
 	}
@@ -74,9 +74,9 @@ void square_blurr(const uchar4* d_in, uchar4* d_sq,  int blurr_amount, const flo
 	d_sq represents output image intensities
 */
 uchar4* square(const uchar4* const h_image, uchar4* const d_image, uchar4 color, size_t numRows, size_t numCols, 
-							size_t &n_numRows, size_t &n_numCols, bool blurr, int blurr_amount, const float* const d_filter, const size_t filterWidth)
+	size_t &n_numRows, size_t &n_numCols, bool blurr, int blurr_amount, const float* const d_filter, const size_t filterWidth)
 {
-	size_t size, newSize;
+  size_t size, newSize;
   const dim3 blockSize(64, 64, 1);  
   const dim3 gridSize(numRows/blockSize.x+1, numCols/blockSize.y+1,1);  
   size = numRows*numCols;
