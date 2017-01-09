41b6d41b3341bdc999922704d6681f3119fdd60b
shilpi75 <shilpi.c75@gmail.com>
2017-01-09 23:43:44 +0530

    Update

--- square_ops.cu
+++ square_ops.cu
@@ -23,50 +23,47 @@ __global__ void square_kernel(uchar4 *d_in, uchar4 * d_out, size_t numRows, size
   else{
     int w = (numRows - numCols) / 2 ;
     if(x >= w && x < width - w)
-      d_out[y*width + x] = d_in[y*numCols + x];
+      d_out[y*width + x] = d_in[y*numCols + x - w];
     else
       d_out[y*width + x] = color;
   }
 }
 
-__global__ 
-void square_blur(const uchar4* d_in, uchar4* d_sq, const float* const d_filter, const int filterWidth, 
-                 size_t numRows, size_t numCols,  size_t n_numRows, size_t n_numCols)
+__global__
+void square_blur(const uchar4* d_in, uchar4* d_blur, uchar4* d_out, size_t numRows, size_t numCols)
 {
-	int y = blockDim.x*blockIdx.x + threadIdx.x;  
-	int x = blockDim.y*blockIdx.y + threadIdx.y;	
-	int index = x*numRows + y;								//previous index of pixel
-	int n_index = x*n_numRows + y;						//new index of pixel
+  int x = blockDim.x * blockIdx.x + threadIdx.x;
+  int y = blockDim.y * blockIdx.y + threadIdx.y;
+  int width = (numRows > numCols)? numRows: numCols;
 
-	if(y >= n_numCols || x >= n_numRows)  		//check out of bound
-	  return;
+  if(x >= width || y >= width)  //check out of bounds
+    return ;
+  
+  if(numCols > numRows)
+  {
+    int w = (numCols - numRows) / 2;
+    if(y >= w && y < width - w)
+      d_out[y * numCols + x] = d_in[(y-w) * numCols + x];
+    else if(y < w)
+      d_out[y * numCols + x] = d_blur[y * numCols + x];
+    else
+      d_out[y * numCols + x] = d_blur[(numRows + y - width) * numCols + x];
 
-	if(y < numCols && x < numRows)								
-	  d_sq[n_index] = d_in[index];
-	else
-	{
-	  int prev_x = x - (n_numRows - numRows);		//finding pixel to blurr
-	  int prev_y = y - (n_numCols - numCols);
-	  int prev_index = prev_x * numRows + prev_y;
+  }
+  else
+  {
+    int w = (numRows - numCols) / 2;
+    if(x >= w && x <= width - w )
+      d_out[y * width + x] = d_in[y * numCols + (x - w)]; 
+    else if(x < w)
+      d_out[y * width + x] = d_blur[y * numCols + x];
+    else
+      d_out[y * width + x] = d_blur[y * numCols + (numCols + x - width)];
+  }
 
-	  uchar4 sum = make_uchar4(0,0,0,225);
-    for(int px = 0; px < filterWidth; px++)		//calculating new pixel intensity
-	  {
-	    for(int py = 0; py < filterWidth; py++)
-	    {
-       	int row = x + px - (filterWidth/2);
-        int col = y + py - (filterWidth/2);
-        row = min( max(0,row), static_cast<unsigned int>(numCols-1));
-        col = min( max(0,col), static_cast<unsigned int>(numRows-1));
-        sum.x+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].x ) );
-        sum.y+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].y ) );
-        sum.z+= d_filter[py*filterWidth+px] * ( static_cast<float>( d_in[prev_index].z ) );
-	    }
-  	}
-
-  	d_sq[n_index] = sum;
-	}
 }
+
+
 
 uchar4* square_image(uchar4* const d_in, size_t &numRows, size_t &numCols, uchar4 color){
 
@@ -76,7 +73,7 @@ uchar4* square_image(uchar4* const d_in, size_t &numRows, size_t &numCols, uchar
   cudaMalloc((void **) &d_out, width * width * sizeof(uchar4));
 
   dim3 block_size(16, 16, 1);
-  dim3 grid_size(width/16 + 1, width/16 + 1, 1);
+  dim3 grid_size(width/block_size.x + 1, width/block_size.y + 1, 1);
 
   square_kernel<<<grid_size, block_size>>>(d_in, d_out, numRows, numCols, color);
 
@@ -87,43 +84,29 @@ uchar4* square_image(uchar4* const d_in, size_t &numRows, size_t &numCols, uchar
   return h_out;   
 }
 
-// TODO: 
-//   1. Modify this to have lesser parameters.
-//   2. Make it work.
-//   3. Also modify the parameters in main
-uchar4* square_blur(uchar4* d_image, size_t numRows, size_t numCols, size_t &n_numRows, size_t &n_numCols, int blurKernelWidth, float blurKernelSigma)
+
+uchar4* square_blur(uchar4* d_image, size_t &numRows, size_t &numCols, int blurKernelWidth, float blurKernelSigma)
 {
-	size_t newSize;
-  const dim3 blockSize(64, 64, 1);  
-  const dim3 gridSize(numRows/blockSize.x+1, numCols/blockSize.y+1,1);  
+	size_t width = (numRows > numCols)? numRows: numCols;
+  const dim3 blockSize(16, 16, 1);  
+  const dim3 gridSize(width/blockSize.x+1, width/blockSize.y+1, 1);  
   
-  if(numCols > numRows)		//setting new cols and rows size
-  {
-    n_numRows = numCols;
-    n_numCols = numCols;
-  }
-  else
-  {
-    n_numCols = numRows; 
-    n_numRows = numRows;
-  }
-  
-  newSize = n_numRows * n_numCols;
-  uchar4* d_sq;
-  cudaMalloc(&d_sq, sizeof(uchar4)*newSize);
-  
-  /*setting the filter: we need to change blurkernelWidth and blurKernelSigma to change the filter */
-  float* h_filter;
-  int filterWidth;
-  setFilter(&h_filter, &filterWidth, blurKernelWidth, blurKernelSigma);
+  uchar4* h_blur = new uchar4[width * width * sizeof(uchar4)];
 
-  float *d_filter; 	//creating device copy of h_filter
-  cudaMalloc(&d_filter, sizeof(float) * blurKernelWidth * blurKernelWidth);
-  cudaMemcpy(d_filter, h_filter, sizeof(float) * blurKernelWidth * blurKernelWidth, cudaMemcpyHostToDevice);
+  //calling vagisha's function
+  h_blur = blur_ops(d_in, numRows, numCols, 13, 3.0);
+  //h_blur = blur_ops(d_in, numRows, numCols, 9, 2.0);
 
-  square_blur<<<gridSize, blockSize>>>(d_image, d_sq, d_filter, filterWidth, numRows, numCols, n_numRows, n_numCols);
+  uchar4* d_blur;
+  cudaMalloc(&d_blur, sizeof(uchar4) * numRows * numCols);
+  cudaMemcpy(d_blur, h_blur, sizeof(uchar4) * numRows * numCols, cudaMemcpyHostToDevice);
 
-  uchar4 *h_out = new uchar4[n_numRows * n_numCols * sizeof(uchar4)];
-  cudaMemcpy(h_out, d_sq, n_numRows * n_numCols * sizeof(uchar4), cudaMemcpyDeviceToHost);
+  uchar4* d_out;
+  cudaMalloc(&d_out, sizeof(uchar4) * width * width);
+
+  square_blur<<<gridSize, blockSize>>>(d_image, d_blur, d_out, numRows, numCols);
+
+  uchar4 *h_out = new uchar4[width * width * sizeof(uchar4)];
+  cudaMemcpy(h_out, d_out, width * width * sizeof(uchar4), cudaMemcpyDeviceToHost);
   return h_out; 
 }
