10dbec2395d77a618e3be56eb3a7e15fffc85ecc
Yash Verma <verma.yash8@gmail.com>
2017-01-10 18:56:44 +0530

    Library...

--- square_ops.cu
+++ square_ops.cu
@@ -84,30 +84,157 @@ uchar4* square_image(uchar4* const d_in, size_t &numRows, size_t &numCols, uchar
   return h_out;   
 }
 
+void zoom(uchar4 *h_in, uchar4 *h_out, size_t numRows, size_t numCols)
+{
+  size_t width = (numRows > numCols)? numRows: numCols;
 
+  double scaleWidth = (double)width/(double)numCols;
+  double scaleHeight = (double)width/(double)numRows;
+
+  for(long cy = 0; cy < width; cy++)
+  {
+    for(long cx = 0; cx < width; cx++)
+    {
+      int pixel = (cy * width) + cx;
+      int y = cy/scaleHeight;
+      int x = cx/scaleWidth;
+      int nearest_pixel = (y * (numCols ) + x );
+      
+      h_out[pixel] = h_in[nearest_pixel];
+
+    }
+  }
+}
+
+void zoom_2(uchar4 *h_in, uchar4 *h_out, size_t numRows, size_t numCols, int scaleFactor)
+{
+  for(long cy = 0; cy < (numRows * scaleFactor); cy++)
+  {
+    for(long cx = 0; cx < (numCols * scaleFactor); cx++)
+    {
+      int pixel = (cy * numCols * scaleFactor) + cx;
+      int y = cy/scaleFactor;
+      int x = cx/scaleFactor;
+      int nearest_pixel = (y * (numCols ) + x );
+      
+      h_out[pixel] = h_in[nearest_pixel];
+    }
+  }
+}
+__global__
+void blur_new(const uchar4* d_in, uchar4* d_blur, uchar4* d_out, size_t numRows, size_t numCols)
+{
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  int width = (numRows > numCols)? numRows: numCols;
+  int w;
+  if(x >= width || y>=width)
+    return;
+  if(numCols > numRows)
+  {
+    w = (numCols - numRows)/2;
+    if(y >= w && y <= width - w)
+      d_out[y * width + x] = d_in[(y-w) * numCols + x];
+    else
+      d_out[y*width + x] = d_blur[y * width + x];
+  }
+
+}
+
+
+/*
+output: a.jpg
+kernel: blur_new
 uchar4* square_blur(uchar4* d_image, size_t &numRows, size_t &numCols, int blurKernelWidth, float blurKernelSigma)
 {
 	size_t width = (numRows > numCols)? numRows: numCols;
-  const dim3 blockSize(16, 16, 1);  
-  const dim3 gridSize(width/blockSize.x+1, width/blockSize.y+1, 1);  
+  //const dim3 blockSize(16, 16, 1);  
+  //const dim3 gridSize(width/blockSize.x+1, width/blockSize.y+1, 1);  
   
-  uchar4* h_blur = new uchar4[width * width * sizeof(uchar4)];
+  // uchar4* h_blur = new uchar4[numRows * numCols * sizeof(uchar4)];
 
-  //calling vagisha's function
-  h_blur = blur_ops(d_image, numRows, numCols, blurKernelWidth, blurKernelSigma);
-  //h_blur = blur_ops(d_in, numRows, numCols, 9, 2.0);
+  // //calling vagisha's function
+  // h_blur = blur_ops(d_image, numRows, numCols, blurKernelWidth, blurKernelSigma);
+  // //h_blur = blur_ops(d_in, numRows, numCols, 9, 2.0);
 
-  uchar4* d_blur;
-  cudaMalloc(&d_blur, sizeof(uchar4) * numRows * numCols);
-  cudaMemcpy(d_blur, h_blur, sizeof(uchar4) * numRows * numCols, cudaMemcpyHostToDevice);
+  // uchar4* d_blur;
+  // cudaMalloc(&d_blur, sizeof(uchar4) * numRows * numCols);
+  // cudaMemcpy(d_blur, h_blur, sizeof(uchar4) * numRows * numCols, cudaMemcpyHostToDevice);
 
-  uchar4* d_out;
+  // uchar4* d_out;
+  // cudaMalloc(&d_out, sizeof(uchar4) * width * width);
+
+  // square_blur<<<gridSize, blockSize>>>(d_image, d_blur, d_out, numRows, numCols);
+
+  uchar4 *h_image = new uchar4[numCols * numRows * sizeof(uchar4)];
+  cudaMemcpy(h_image, d_image, numCols * numRows * sizeof(uchar4), cudaMemcpyDeviceToHost);
+  
+  uchar4 *h_zoom = new uchar4[width * width * sizeof(uchar4)];
+  zoom(h_image, h_zoom, numRows, numCols);
+  
+
+  uchar4 *d_in, *d_blur, *h_blur;
+  cudaMalloc(&d_in, sizeof(uchar4) * width * width);
+  cudaMemcpy(d_in, h_zoom, sizeof(uchar4) * width * width, cudaMemcpyHostToDevice );
+
+  cudaMalloc(&d_blur, sizeof(uchar4) * width * width);
+  h_blur = new uchar4[width * width * sizeof(uchar4)];
+
+  h_blur = blur_ops(d_in, width, width, blurKernelWidth, blurKernelSigma);
+  
+  cudaMemcpy(d_blur, h_blur, sizeof(uchar4) * width * width, cudaMemcpyHostToDevice);
+  dim3 threads(16, 16, 1);
+  dim3 blocks(width/threads.x, width/threads.y, 1);
+
+  uchar4 *d_out;
   cudaMalloc(&d_out, sizeof(uchar4) * width * width);
+  blur_new<<<blocks, threads>>>(d_image, d_blur, d_out, numRows, numCols);
 
-  square_blur<<<gridSize, blockSize>>>(d_image, d_blur, d_out, numRows, numCols);
+  uchar4 *h_out = new uchar4[sizeof(uchar4) * width * width];
+  cudaMemcpy(h_out, d_out, width * width * sizeof(uchar4), cudaMemcpyDeviceToHost);
+
 
   numRows = numCols = width;
-  uchar4 *h_out = new uchar4[width * width * sizeof(uchar4)];
-  cudaMemcpy(h_out, d_out, width * width * sizeof(uchar4), cudaMemcpyDeviceToHost);
-  return h_out; 
+  return h_out;
+  
+  //uchar4 *h_out = new uchar4[width * width * sizeof(uchar4)];
+  //cudaMemcpy(h_out, d_out, width * width * sizeof(uchar4), cudaMemcpyDeviceToHost);
+  //return h_out; 
+}
+*/
+#include<iostream>
+uchar4* square_blur(uchar4* d_image, size_t &numRows, size_t &numCols, int blurKernelWidth, float blurKernelSigma)
+{
+  uchar4 *h_image = new uchar4[numCols * numRows * sizeof(uchar4)];
+  cudaMemcpy(h_image, d_image, numRows * numCols * sizeof(uchar4), cudaMemcpyDeviceToHost);
+
+  if(numCols > numRows)
+  {
+    int scaleFactor = numCols/numRows + 1;
+    size_t newSize = numCols * numRows * scaleFactor * scaleFactor;
+
+    std::cout<<"Size: "<<newSize;
+    std::cout<<"rows: "<<numRows * scaleFactor;
+    std::cout<<"Cols: "<<numCols * scaleFactor;
+    std::cout<<"factor: "<<scaleFactor;
+
+    uchar4 *h_zoom = new uchar4[sizeof(uchar4) * newSize];
+
+    zoom_2(h_image, h_zoom, numRows, numCols, scaleFactor);
+
+
+    numRows = numRows * scaleFactor;
+    numCols = numCols * scaleFactor;
+
+    return h_zoom;
+
+    uchar4 *d_zoom;
+    cudaMalloc(&d_zoom, sizeof(uchar4) * newSize);
+    cudaMemcpy(d_zoom, h_zoom, sizeof(uchar4) * newSize, cudaMemcpyHostToDevice);
+
+    //blur_ops(d_zoom, h_blur, (size_t)(numRows * scaleFactor), (size_t)(numCols * scaleFactor), blurKernelWidth, blurKernelSigma);
+
+  }
+
 }
